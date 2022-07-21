47998a814495d036c7034496ebbe6d040905544f
Juan David Delgado Guzman <juandavid9822@hotmail.es>
2022-07-20 22:58:15 -0500

    200722-7

--- components/Education.jsx
+++ components/Education.jsx
@@ -10,7 +10,7 @@ const Education = ({ data }) => {
             <VStack align={'start'}>
                 <Text fontWeight={600}>{data?.degree}</Text>
                 <Text>{data?.school}</Text>
-                <Text color={'gray.400'}>{data?.startDate} - {data?.endDate}</Text>
+                <Text color={'gray.400'}>{new Date(data?.startDate).getFullYear()} - {new Date(data?.endDate).getFullYear()}</Text>
                 <Text color={'gray.800'}>{data?.descDegree}</Text>
             </VStack>
         </HStack>
